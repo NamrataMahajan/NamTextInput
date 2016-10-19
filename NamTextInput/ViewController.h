0b2ece2c228c5c856ffa3a7e4e76ea93c4b00ba0
Namrata Mahajan <nammahajan12@gmail.com>
2016-10-19 21:07:27 +0530

    initial commit

--- NamTextInput/ViewController.h
+++ NamTextInput/ViewController.h
@@ -7,8 +7,23 @@
 //
 
 #import <UIKit/UIKit.h>
+#define kHorizontalPadding 20.0
+#define kVerticalPadding 20.0
+#define kAllUIElementHeight 50.0
+#define klogButtonWidth 100.0
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITextFieldDelegate>
+{
+    CGFloat screenWidth;
+    CGFloat screnHeight;
+    CGFloat  displayLabelYCoordinate;
+    CGFloat textFieldWidth;
+    CGFloat logButtonXCoordinate;
+    UILabel *displayLabel;
+    UIButton *logButton;
+    UITextField *myTextField;
+    
+}
 
 
 @end
--- NamTextInput/ViewController.m
+++ NamTextInput/ViewController.m
@@ -17,11 +17,56 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    screenWidth =[[UIScreen mainScreen]bounds].size.width;
+    screnHeight =[[UIScreen mainScreen]bounds].size.height;
+    textFieldWidth =screenWidth-(3*kHorizontalPadding)-kAllUIElementHeight;
+    logButtonXCoordinate = textFieldWidth +(2*kHorizontalPadding);
+    displayLabelYCoordinate = kVerticalPadding +kAllUIElementHeight +100;
+    myTextField = [[UITextField alloc]initWithFrame:CGRectMake(kHorizontalPadding, 100, textFieldWidth, kAllUIElementHeight)];
+    [myTextField setBorderStyle:UITextBorderStyleBezel];
+    myTextField.delegate= self;
+    [self.view addSubview:myTextField];
+
+    
+    logButton =[[UIButton alloc]initWithFrame:CGRectMake(logButtonXCoordinate, 100, kAllUIElementHeight, kAllUIElementHeight)];
+    logButton.backgroundColor = [UIColor redColor];
+    [logButton setTitle:@"Log" forState:UIControlStateNormal];
+    logButton.layer.cornerRadius = kAllUIElementHeight/2;
+    
+    [logButton addTarget:self action:@selector(handlelog) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:logButton];
+    
+    displayLabel = [[UILabel alloc]initWithFrame:CGRectMake(kHorizontalPadding, displayLabelYCoordinate, screenWidth - (2*kHorizontalPadding), kAllUIElementHeight)];
+    
+    displayLabel.backgroundColor = [UIColor cyanColor];
+    displayLabel.textColor =[UIColor blackColor];
+    displayLabel.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:displayLabel];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+-(void)handlelog {
+    [self displaContent:myTextField.text];
+    
+}
+-(void)displaContent:(NSString *)content {
+    if (content.length>0) {
+        displayLabel.text = content;
+    }
+    else {
+        displayLabel.text = @"Please enter value.";
+        
+    }
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self displaContent:textField.text];
+    [textField resignFirstResponder];
+    return YES;
 }
 
 @end
